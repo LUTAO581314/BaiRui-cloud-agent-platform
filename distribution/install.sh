2514609f975981933222218a6d6314b182862751
MOXI <3420615279@qq.com>
2026-07-17 18:36:43 +0800

    fix: include Docker client in Server Agent image

--- distribution/install.sh
+++ distribution/install.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 readonly REPOSITORY="LUTAO581314/BaiRui-cloud-agent-platform"
-readonly INSTALLER_VERSION="0.1.0-rc.4"
+readonly INSTALLER_VERSION="0.1.0-rc.5"
 
 RELEASE_VERSION="$INSTALLER_VERSION"
 DOMAIN=""
